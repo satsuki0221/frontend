7defc4b44ce80befdece0232da4eb17d8997678a
Yugo Matsumoto <matsumoto@q-co.jp>
2016-10-12 11:50:02 +0900

    処理の改善

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,9 @@ var babel       = require("gulp-babel");
 var plumber     = require('gulp-plumber');
 
 gulp.task('default', function(){
-	gulp.run('watch');
-	gulp.run('browserSync');
+	gulp.watch('./public/sass/**/*.scss', ["compass"]);
 	gulp.watch("./public/es6/**/*.js", ["js"]);
+	gulp.run('browserSync');
 });
 
 
@@ -18,29 +18,26 @@ gulp.task("js", function() {
 		.pipe(babel({
 			presets: ['es2015']
 		}))
-		.pipe(gulp.dest('public/js'));
+		.pipe(gulp.dest('./public/js'));
 });
-
-var reload = browserSync.reload;
 
 gulp.task('browserSync', function() {
 	browserSync.init({
-		files: ['./public/**/*.html','./public/**/*.css','./public/js/**/*.js','.public/**/*.php'],
+		files: [
+			'./public/**/*.html',
+			'./public/**/*.css',
+			'./public/js/**/*.js',
+			'./public/**/*.php'
+		],
 		proxy: 'http://mylocal.com/frontend/public/',
 	});
 });
 
 gulp.task('compass', function(){
-	gulp.src('public/sass/**/*.scss').pipe(compass({
-		config_file: 'public/config.rb',
+	gulp.src('./public/sass/**/*.scss').pipe(compass({
+		config_file: './public/config.rb',
 		comments: false,
-		css: 'public/stylesheets/',
-		sass: 'public/sass/'
+		css: './public/stylesheets/',
+		sass: './public/sass/'
 	}));
-});
-
-gulp.task('watch', function(){
-	gulp.watch('public/sass/**/*.scss', function(event) {
-		gulp.run('compass');
-	});
 });
